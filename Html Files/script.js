513cf0c390749357448dbe7f0d8f803e628c01bd
muzamil-dev-313 <karbalaimuzammil50@gmail.com>
2026-02-15 13:55:34 +0500

    Update script.js

--- Html Files/script.js
+++ Html Files/script.js
@@ -1,2 +1,2 @@
 alert("Hey be careful this website may harmful for you")
-console.log("Welcome to My github account https://github.com/muzamil-dev-313");
+console.log("Welcome to my Github account https://github.com/muzamil-dev-313");
