117a3f62c9eccf034721dfc7cd02b79c376764bc
Muzamil <karbalaimuzammil50@gmail.com>
2026-02-24 23:53:00 +0500

    Add files via upload

---
+++ First JavaScript/first.js
@@ -0,0 +1,32 @@
+alert("This site may harmful for you !")
+
+console.log("This is my first JavaScript file to write a code.")
+
+Name = "Muzamil" 
+age = 21
+console.log(Name);
+console.log(age);                   
+                  
+a = null
+console.log(a)
+b = undefined;
+console.log(b)
+
+x = prompt("Enter your phone number")
+// var x = prompt("Enter your phone number")
+
+console.log("Your phone number is", x)
+
+age = prompt("Enter your age")
+
+if(age > 18) {
+    console.log("You can drive a car.")
+}
+else {
+    console.log("You can't drive a car.")
+}
+
+document.title = "JavaScript"
+
+document.body.style.color = "yellow"
+document.body.style.backgroundColor = "dodgerblue"
