ae9ebe58c7a7ae147b1685235fa8eed9733c2c68
Muzamil <karbalaimuzammil50@gmail.com>
2026-03-12 01:12:37 +0500

    Create exercise.js

---
+++ JS If-else Condition/exercise.js
@@ -0,0 +1,82 @@
+// Write a program that checks if a number is positive or negative
+let number = 0;
+
+if (number > 0) {
+    console.log("The number is positive.")
+}
+else if (number < 0) {
+    console.log("The number is negative.")
+}
+else {
+    console.log("The number is zero.")
+}
+
+// Ask the user for their age and check if they are eligible to vote (18 or older).
+let age = 17;
+
+if (age >= 18) {
+    console.log("You are eligible to vote.")
+}
+else {
+    console.log("You are Not eligible to vote.")
+}
+
+// Write a program that checks if a number is even or odd.
+let num = 9;
+
+if (num % 2 == 0) {
+    console.log("The number is even.")
+}
+else {
+    console.log("The number is odd.")
+}
+
+// Check if a number is greater than 100 or not.
+let n = 100
+
+if (n > 100) {
+    console.log("The number is greater than 100.")
+}
+else {
+    console.log("The number is not greater than 100.")
+}
+
+// Ask the user for a temperature and print:
+// "Hot" if above 30
+// "Cold" otherwise
+
+let temperature = 31;
+
+if (temperature > 30) {
+    console.log("The Weather is Hot!")
+}
+else {
+    console.log("The Weather is Cold!")
+}
+
+// Write a program to check if a number is divisible by 5.
+
+let i = 25;
+
+if (i % 5 == 0) {
+    console.log("The number is divisible by 5.")
+}
+else {
+    console.log("The number is not divisible by 5.")
+}
+
+
+// Write a program to find the largest of two numbers.
+let first_number = 12;
+let second_number = 21;
+
+if (first_number > second_number) {
+    console.log("largest number is", first_number)
+}
+else if (second_number > first_number) {
+    console.log("Largest number is", second_number)
+}
+else {
+    console.log("Both are same numbers")
+}
+
