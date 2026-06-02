86e635bce959fe74830870f405b4e98b2f9f9c22
Muzamil <karbalaimuzammil50@gmail.com>
2026-06-02 09:51:15 -0700

    Update script.js

--- Array JS/script.js
+++ Array JS/script.js
@@ -66,16 +66,25 @@ console.log(`Avg marks of the class = ${avg}`)
 
 // Qs. For a given array with prices of 5 items -> [250, 645, 300, 900, 50] All items have an offer of 10% OFF on them. 
 // Change the array to store final price after applying offer.
-let items = [250, 645, 300, 900, 50];
+// My logic
+let itms = [250, 645, 300, 900, 50];
 
-for (let index = 0; index < items.length; index++) {
-    discount = items[index] * 10 / 100;
-    final_price = items[index] - discount
+for (let index = 0; index < itms.length; index++) {
+    discount = itms[index] * 10 / 100;
+    final_price = itms[index] - discount
     // console.log(final_price)
-    items[index] = final_price
+    itms[index] = final_price
 }
-console.log(items)
-
+console.log(itms)
+// Logic of Apna College 
+let items = [250, 645, 300, 900, 50 ];
+let i = 0;
+for(let val of items) {
+    let offer = val / 10;
+    items[i] = items[i] - offer;
+    console.log(`value after offer = ${items[i]}`);
+    i++;
+}
 
 // Find maximum number:
 let n = [12, 45, 7, 89, 23];
