cdcf6fc043803b7f43fafde52b07aebb3987d170
Muzamil <karbalaimuzammil50@gmail.com>
2026-06-10 22:51:34 -0700

    Update script.js

--- Loops/script.js
+++ Loops/script.js
@@ -95,7 +95,6 @@ for(let i = 0; i <= 100; i++){
     }
 }
 
-
 // Create a game where you start with any random game number. Ask the user to keep guessing the game number until the user enters correct value.
 
 let guessnumber = 55;
@@ -106,3 +105,68 @@ while (user != guessnumber) {
     
 }
 console.log("Congratulation you have been found the lucky number. It's", i)
+
+// Q1. Print numbers from 1 to 10 using a for loop. 
+for (i = 1; i <= 10; i++) {
+    console.log(i);
+};
+
+// Q2. Print numbers from 10 to 1 using a while loop. 
+i = 11;
+while (i > 0) {
+    i--;
+    console.log(i);
+};
+
+// Q3. Print even numbers from 1 to 20 using a for loop. 
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    };
+};
+
+// Q4. Print odd numbers from 1 to 15 using a while loop.
+i = 1;
+while (i <= 15) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+    i++;
+};
+
+// Q5. Print the multiplication table of 5 (i.e.5x1 = 5 ...,5 x 10 = 50)
+for (let i = 1; i < 11; i++) {
+    console.log(`5 * ${i} = ${i * 5}`);
+};
+
+// Q6. Find the sum of numbers from 1 to 100 using a loop. 
+sum = 0;
+for (let i = 0; i <= 100; i++) {
+    sum += i;
+};
+console.log(sum);
+
+// Q7. Print all numbers between 1 to 50 that are divisible by 3 
+for (let index = 1; index <= 50; index++) {
+    if (index % 3 === 0) {
+        console.log(index);
+    };
+};
+
+// Q8. Ask the user for a number and print whether each number from 1 to that number is even or odd. (e.g., "1" is odd" "2" is even" ,...) 
+let x = prompt("Enter a number: ");
+for (let i = 0; i < x; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} is even.`);
+    } else{    
+        console.log(`${i} is odd`);
+    }
+};
+
+// Q9. Count how many numbers between 1 to 100 are divisible by both 3 and 5.
+
+for (let i = 1; i < 101; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} is divisible by 3 and 5`);
+    };
+};
